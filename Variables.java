adf7988e1cb8552e7a2f54b34ca73be7f001b052
Rohithreddysangireddy <rohithreddysangireddy@gmail.com>
2025-12-10 15:26:15 +0530

    variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables{
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int number = 10;
+        String name = "John";
+        double height = 5.9;
+
+        // Printing the variables
+        System.out.println("Number: " + number);
+        System.out.println("Name: " + name);
+        System.out.println("Height: " + height);
+    }
+}
